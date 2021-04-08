d6e618bec4476530bd80bf9fd2fb2801fe7e29da
vishnu <vishnudevxamarin.gmail.com>
2021-04-08 20:47:17 +0530

    Add project files.

---
+++ ProNobrokerHood/ProNobrokerHood/views/HomePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProNobrokerHood.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        ObservableCollection<String> ts { get; set; } = new ObservableCollection<string>();
+        public HomePage()
+        {
+            InitializeComponent();
+            ts.Add("");
+            ts.Add("");
+            ts.Add("");
+            ts.Add("");
+            ts.Add("");
+            ts.Add("");
+            ts.Add("");
+            ts.Add("");
+            lstOfHomePage.ItemsSource = ts;
+        }
+    }
+}
---
+++ ProNobrokerHood/ProNobrokerHood/views/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProNobrokerHood.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void VaidateMobileNumber(object sender, EventArgs e)
+        {
+            var mobilenumber = ety_mobile.Text;
+            if (string.IsNullOrEmpty(mobilenumber))
+            {
+                DisplayAlert("Error Title", "Enter Your userName or Mobile Number", "ok");
+            }
+            else { 
+            // check entried value is mobile or not 
+            //then we need to procced wit Mobile Number and save it.
+            }
+        }
+    }
+}
